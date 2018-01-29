8d8e82a320d7de53436c109cd953ca646c1161b8
Aleksey Ivanov <aleksej.ivanov.12@mail.ru>
2018-01-29 14:48:55 +0300

    Adding class Person

---
+++ src/com/company/Person.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Person extends  Her{
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person(int id) {
+        super(id);
+    }
+}
